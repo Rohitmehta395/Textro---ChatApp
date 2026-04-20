0052a603e421630be8b15ceb1a2420feaba9a312
Rohit Mehta <rohitmehtaddn@gmail.com>
2026-04-20 21:06:22 +0530

    Implement login and logout functionality in auth controller and routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -65,3 +65,50 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Invalid server error." });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    generateToken(user._id, res);
+
+    return res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in login controller: ", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: ENV.NODE_ENV === "development" ? false : true,
+    });
+    return res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.error("Error in logout controller: ", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
